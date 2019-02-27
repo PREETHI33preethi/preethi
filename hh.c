ed166ffcf9c5534276cd9561481ad616b3f606d2
PREETHI33preethi <48052863+PREETHI33preethi@users.noreply.github.com>
2019-02-27 16:38:07 +0530

    Update hh.c

--- hh.c
+++ hh.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-
-int main()
+#include<iostream.h>
+#include<conio.h>
+void main()
 {
-    printf("Hello World");
-
-    return 0;
+ clrscr();
+ int number;
+ cout<<"Enter a number : ";
+ cin>>number;
+ if(number>0)
+   cout<<"Number is Positive";
+ else if(number<0) 
+   cout<<"Number is Negative";
+ else
+   cout<<"Number is Zero";
+ getch();
 }
